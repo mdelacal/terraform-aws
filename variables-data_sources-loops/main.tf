2d63d1a21ece0ff15b61fd1aba16b8b2ce046e8d
Miguel de la Cal Bravo <miguelcal97@gmail.com>
2020-07-12 17:44:44 +0200

    Terraform variables - data sources - loops

--- variables-data_sources-loops/main.tf
+++ variables-data_sources-loops/main.tf
@@ -13,11 +13,44 @@ resource "aws_vpc" "main1" {
     }
 }
 
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "subnets" {
+    # count = "${length(var.azs_list)}"
+    # availability_zone = "${element(var.azs_list, count.index)}"
+    
+    count = "${length(data.aws_availability_zones.azs.names)}"
+    availability_zone = "${element(data.aws_availability_zones.azs.names, count.index)}"
+
     vpc_id = "${aws_vpc.main1.id}"
-    cidr_block = "${var.subnet_cidr}"
+    cidr_block = "${element(var.subnet_cidr, count.index)}"
 
     tags = {
-        Name = "Subnet1"
+        Name = "Subnet-${count.index + 1}"
     }
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = "${aws_vpc.main1.id}"
+
+    tags = {
+        Name = "VPC Main"
+    }
+}
+
+resource "aws_route_table" "web-public-rt" {
+    vpc_id = "${aws_vpc.main1.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+
+    tags = {
+        Name = "Public Subnet RT"
+    }
+}
+
+resource "aws_route_table_association" "public-subnet" {
+    count = "${length(var.subnet_cidr)}"
+    subnet_id = "${element(aws_subnet.subnets.*.id, count.index)}"
+    route_table_id = "${aws_route_table.web-public-rt.id}"
 }
--- variables-data_sources-loops/variables.tf
+++ variables-data_sources-loops/variables.tf
@@ -7,5 +7,17 @@ variable "vpc_cidr" {
 }
 
 variable "subnet_cidr" {
-    default = "192.168.1.0/24"
+    type = "list"
+    default = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+}
+
+# Availability zones
+variable "azs_list" {
+    type = "list"
+    default = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
+
+# Declare the data source
+data "aws_availability_zones" "azs" {
+  state = "available"
 }
